3786bfa78a1034b6ae8feb18eb0b783e5ec919a5
ABuBakar7447 <lone7447wolf@gmail.com>
2022-11-10 16:01:28 +0600

    single service details now can be loaded after view click button

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
+import { Link } from 'react-router-dom';
 
 const ServiceDetails = () => {
-    const {service_name} = useLoaderData()
+    const {_id, service_name, image_url, description, rating, price} = useLoaderData()
     return (
         <div>
-            <h1>{service_name} </h1>
+            <div className="card card-compact  bg-base-100 shadow-xl mx-auto w-3/4 my-10">
+                <PhotoProvider>
+                    <PhotoView src={image_url}>
+                    <figure><img  src={image_url} alt="Shoes" /></figure>
+                    </PhotoView>
+                </PhotoProvider>
+                <div className="card-body">
+                    <h2 className="card-title">{service_name}</h2>
+                    <h3 className='font-bold'>Price: ${price}</h3>
+                    <h3 className='font-bold'>Rating: {rating}</h3>
+                    <p>{description}</p>
+                    
+                </div>
+            </div>
         </div>
     );
 };
